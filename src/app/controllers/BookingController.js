01896134c04a02d01313bff0b5a7f2960d18e3f5
Willian Home <willian@wiki4fit.com.br>
2019-10-04 17:54:07 -0300

    Tratamento de erro no Booking e Dashboard

--- src/app/controllers/BookingController.js
+++ src/app/controllers/BookingController.js
@@ -2,22 +2,26 @@ const { Booking } = require('../models')
 
 class BookingController {
   async store(req, res) {
-    const { user_id } = req.headers
-    const { spot_id } = req.params
-    const { date } = req.body
+    try {
+      const { user_id } = req.headers
+      const { spot_id } = req.params
+      const { date } = req.body
 
-    const booking = await Booking.create({
-      user: user_id,
-      spot: spot_id,
-      date
-    })
+      const booking = await Booking.create({
+        user: user_id,
+        spot: spot_id,
+        date
+      })
 
-    await booking
-      .populate('spot')
-      .populate('user')
-      .execPopulate()
+      await booking
+        .populate('spot')
+        .populate('user')
+        .execPopulate()
 
-    return res.json(booking)
+      return res.json(booking)
+    } catch (error) {
+      return res.status(400).json({ error })
+    }
   }
 }
 
--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -2,11 +2,15 @@ const { Spot } = require('../models')
 
 class DashboardController {
   async show(req, res) {
-    const { user_id } = req.headers
+    try {
+      const { user_id } = req.headers
 
-    const spots = await Spot.find({ user: user_id })
+      const spots = await Spot.find({ user: user_id })
 
-    return res.json(spots)
+      return res.json(spots)
+    } catch (error) {
+      return res.status(400).json({ error })
+    }
   }
 }
 
